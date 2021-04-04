1f7a64a5bed5a6d8062f9dd93baab451cd00dade
skylarkgit <abhay199658@gmail.com>
2021-04-04 22:35:28 +0530

    [add] scroll indicator

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 AOS.init();
+
+document.getElementById('scroll-down').addEventListener('click', () => {
+    window.scrollBy({
+        top: window.innerHeight,
+        behavior: 'smooth'
+    });
+})
